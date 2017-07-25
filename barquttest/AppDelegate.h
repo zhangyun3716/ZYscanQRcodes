e5a6412bd9716a14665456e6ad5576b6835b0193
zhangyun3716 <619319732@qq.com>
2017-07-25 15:30:39 +0800

    Initial Commit

---
+++ barquttest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  barquttest
+//
+//  Created by flexium on 2017/7/25.
+//  Copyright © 2017年 FLEXium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ barquttest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  barquttest
+//
+//  Created by flexium on 2017/7/25.
+//  Copyright © 2017年 FLEXium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
